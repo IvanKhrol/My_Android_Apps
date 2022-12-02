234928142774f3c7a1f0b5e0252bd8a55f65dcaa
IvanKhrol <khrol_ivan@mail.ru>
2022-12-02 04:14:15 +0300

    change constructot fot class Field and add TODO

--- Diary/app/src/main/java/com/example/diary/Field.java
+++ Diary/app/src/main/java/com/example/diary/Field.java
@@ -1,37 +1,66 @@
 package com.example.diary;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+TODO: 
+//может быть лучше все поля педелать в sting?
 
 public class Field {
-    LocalDate date;
-    int time;
+    String date[] = new String[3];    // date format "dd.mm.yy"
+    String time[] = new String[2];    // time format "hh:mm"
     int sugar;
     int XE;
     int insulin;
 
-    Field () {
-        date  = LocalDate.of(0, 0, 0);
-        sugar = 0;
-        XE = 0;
-        insulin = 0;
+//==================================================================================================================================
+//                                                           Сonstructor
+//==================================================================================================================================
+
+
+    Field() {
+        for (int i = 0; i < 2; ++i) {
+            this.date[i] = "00";
+            this.time[i] = "00";
+        }
+        this.date[2] = "00";
+        this.sugar = 0;
+        this.XE = 0;
+        this.insulin = 0;
     }
 
-    Field (LocalDate d, int sug) {
-        date  = d;
-        sugar = s;
+    Field(LocalDateTime d, int sug) {
+        this()
+        this.date[0] = d.getDayOfMonth().toString();
+        this.date[1] = d.getYear().toString();
+        this.date[2] = d.getMonthValue().toString();
+        this.time[0] = d.getHour().toString();
+        this.time[1] = d.getMinute().toString();
+        this.sugar = sug;
     }
 
-    Field (LocalDate d, int sug, int ins) {
-        date  = d;
-        sugar = s;
-        insulin = ins;
+    Field(LocalDateTime d, int sug, int ins) {
+        this(d, sug)
+        this.insulin = ins;
     }
 
-    Field (LocalDate d, int sug, int XE, int ins) {
-        date  = d;
-        sugar = s;
+    Field(LocalDateTime d, int sug, int XE) {
+        this(d, sug)
         this.XE = XE;
-        insulin = ins;
     }
 
+    Field(LocalDateTime d, int sug, int XE, int ins) {
+        this(d, sug)
+        this.XE = XE;
+        this.insulin = ins;
+    }
+
+//==================================================================================================================================
+//                                                              Functions
+//==================================================================================================================================
+
+
+    void display() {
+        System.out.printf("%s.%s.%s | %s:%s | %d | %d | %d\n", date[0], date[1], date[2], time[0], time[1] sugar, XE, insulin);
+    }
 }
+
 
